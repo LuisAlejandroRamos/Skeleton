cd2b94213b9c2b359ce868f0eb7399ca2d11107b
LuisAlejandroRamos <104103476+LuisAlejandroRamos@users.noreply.github.com>
2022-11-16 10:32:53 -0600

    first commit

---
+++ pasarela-de-pagos/pasarela-de-pago.php
@@ -0,0 +1,16 @@
+<?php
+/**
+*
+Plugin Name:       pasarela-de-pago
+Plugin URI:        https://example.com/plugins/the-basics/
+Description:       Esto es una pasarela de pago en proceso
+Version:           1.0.1
+Requires at least: 5.2
+Requires PHP:      7.2
+Author:            Luis Ramos
+Author URI:        https://author.example.com/
+License:           GPL v2 or later
+License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+Update URI:        https://example.com/my-plugin/
+*/ 
+?>
